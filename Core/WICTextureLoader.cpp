547598dbe491c336f503d12b231f9ad533e1c760
Chuck Walbourn <chuckw@windows.microsoft.com>
2017-07-03 16:06:48 -0700

    WIC format converter should use WICBitmapPaletteTypeMedianCut

--- Core/WICTextureLoader.cpp
+++ Core/WICTextureLoader.cpp
@@ -554,7 +554,7 @@ namespace
                     return E_UNEXPECTED;
                 }
 
-                hr = FC->Initialize( scaler.Get(), convertGUID, WICBitmapDitherTypeErrorDiffusion, 0, 0, WICBitmapPaletteTypeCustom );
+                hr = FC->Initialize( scaler.Get(), convertGUID, WICBitmapDitherTypeErrorDiffusion, nullptr, 0, WICBitmapPaletteTypeMedianCut );
                 if ( FAILED(hr) )
                     return hr;
 
@@ -582,7 +582,7 @@ namespace
                 return E_UNEXPECTED;
             }
 
-            hr = FC->Initialize( frame, convertGUID, WICBitmapDitherTypeErrorDiffusion, 0, 0, WICBitmapPaletteTypeCustom );
+            hr = FC->Initialize( frame, convertGUID, WICBitmapDitherTypeErrorDiffusion, nullptr, 0, WICBitmapPaletteTypeMedianCut );
             if ( FAILED(hr) )
                 return hr;
 
