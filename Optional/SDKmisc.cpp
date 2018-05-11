35034f03506edb78f7e54f15715dbf7481789070
Chuck Walbourn <walbourn@users.noreply.github.com>
2018-05-11 11:41:58 -0700

    noexcept and initialization cleanup (#11)

--- Optional/SDKmisc.cpp
+++ Optional/SDKmisc.cpp
@@ -904,12 +904,15 @@ HRESULT CDXUTResourceCache::OnDestroyDevice()
 //======================================================================================
 
 _Use_decl_annotations_
-CDXUTTextHelper::CDXUTTextHelper( ID3D11Device* pd3d11Device, ID3D11DeviceContext* pd3d11DeviceContext, CDXUTDialogResourceManager* pManager, int nLineHeight )
+CDXUTTextHelper::CDXUTTextHelper( ID3D11Device* pd3d11Device, ID3D11DeviceContext* pd3d11DeviceContext, CDXUTDialogResourceManager* pManager, int nLineHeight ) :
+    m_clr(0, 0, 0, 0),
+    m_pt{ 0, 0 },
+    m_nLineHeight{},
+    m_pd3d11Device(pd3d11Device),
+    m_pd3d11DeviceContext(pd3d11DeviceContext),
+    m_pManager(pManager)
 {
     Init( nLineHeight );
-    m_pd3d11Device = pd3d11Device;
-    m_pd3d11DeviceContext = pd3d11DeviceContext;
-    m_pManager = pManager;
 }
 
 CDXUTTextHelper::~CDXUTTextHelper()
