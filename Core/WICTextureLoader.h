f3b8c48e30033066034d74ad98cb98d2e22ae25e
Chuck Walbourn <chuckw@microsoft.com>
2021-06-02 17:14:32 -0700

    Resync'd DDSTextureLoader, ScreenGrab, WICTextureLoader

--- Core/WICTextureLoader.h
+++ Core/WICTextureLoader.h
@@ -25,6 +25,7 @@
 
 #include <d3d11_1.h>
 
+#include <cstddef>
 #include <cstdint>
 
 namespace DirectX
@@ -41,6 +42,17 @@ namespace DirectX
         WIC_LOADER_MAKE_SQUARE  = 0x40,
         WIC_LOADER_FORCE_RGBA32 = 0x80,
     };
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-dynamic-exception-spec"
+#endif
+
+    DEFINE_ENUM_FLAG_OPERATORS(WIC_LOADER_FLAGS);
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
 #endif
 
     // Standard version
